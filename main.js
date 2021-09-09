715609e868f91357941736fd5abec66cc0cd2884
JunBBang2 <leejy91945@naver.com>
2021-09-09 15:34:19 +0900

    JS Transparent

--- main.js
+++ main.js
@@ -36,7 +36,28 @@ homeContactBtn.addEventListener('click', () => {
 });
 
 
+
+//Home Scroll에 따른 Home 투명도 변환
+
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () =>{
+    home.style.opacity = 1-window.scrollY/homeHeight;
+});
+
+
+
+
+
+
+
+
+
+
+
 function scrollIntoView(selector){
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior: 'smooth'});
 }
+
+
