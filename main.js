fd7f03675ce8f3641a0e7fb1255d82638d72e6ea
JunBBang2 <leejy91945@naver.com>
2021-09-09 15:14:18 +0900

    JS : Navbar Menu Click

--- main.js
+++ main.js
@@ -13,3 +13,18 @@ document.addEventListener('scroll',() =>{
 
     }
 }); 
+
+
+//Navbar btn 눌렀을때 원하는 Section으로 Scrolling
+
+const navbarMenu =document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) =>{
+ const target = event.target;
+ const link = target.dataset.link;
+ if (link == null){
+     return;
+ }
+
+  const scroollTo = document.querySelector(link);
+  scroollTo.scrollIntoView({behavior: 'smooth'});
+});
